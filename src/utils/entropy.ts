68234683244bc124d5d84a50f4b6e7429bf99d6c
markusenglund <markus.s.englund@gmail.com>
2025-06-07 18:52:36 +0200

    Use lowest entropy of all numerators when calculating entropy

--- src/utils/entropy.ts
+++ src/utils/entropy.ts
@@ -1,22 +1,34 @@
+import { roundFloatingPointInaccuracies } from "./roundFloatingPointInaccuracies";
+
 export function calculateNumberEntropy(value: number): number {
   // Values that are common years should receive an entropy score of 100
   if (value >= 1900 && value <= 2030 && Number.isInteger(value)) {
     return 100;
   }
 
+  const denominators = [2, 3, 7, 9, 11, 13, 17, 19, 23];
+  const rawBaseNumberEntropy = calculateRawNumberEntropy(value);
+  const numeratorEntropies = denominators.map(denominator => {
+    const numerator = value * denominator;
+    const roundedNumerator = roundFloatingPointInaccuracies(numerator, 8, 1e-8);
+    const roundedNumeratorEntropy = calculateRawNumberEntropy(roundedNumerator);
+    return roundedNumeratorEntropy;
+  });
+  const minNumeratorEntropy = Math.min(...numeratorEntropies);
+  if (minNumeratorEntropy < rawBaseNumberEntropy / 2) {
+    return minNumeratorEntropy;
+  }
+  return rawBaseNumberEntropy;
+}
+
+function calculateRawNumberEntropy(value: number): number {
   // Convert numbers with decimal points to integers by simply removing the decimal point but keeping the digits
   const str = value.toString();
   const withoutPoint = str.replace(".", "");
   // Remove trailing zeroes
   const withoutTrailingZeroes = withoutPoint.replace(/0+$/, "");
-  const withoutOneTrailingFive = withoutTrailingZeroes.replace(/5$/, "");
 
-  // If a number has 4 or more repeating digits, only the first in the repeating sequence is kept
-  const withoutRepeatingDigits = withoutOneTrailingFive.replace(
-    /(\d)\1{3,}/g,
-    (_, digit) => digit
-  );
-  const entropy = parseInt(withoutRepeatingDigits || "0");
+  const entropy = parseInt(withoutTrailingZeroes || "0");
   return entropy;
 }
 
@@ -40,4 +52,3 @@ export function calculateSequenceEntropyScore(values: number[]): number {
   }, 0);
   return sum;
 }
-
