6c4413f185668031a5d648b4ef49630f581fb2b2
markusenglund <markus.s.englund@gmail.com>
2025-07-03 17:51:32 +0200

    Improve retry logic

--- src/dryad/listDatasets.ts
+++ src/dryad/listDatasets.ts
@@ -20,34 +20,34 @@ export async function listDatasets({
     per_page: String(perPage),
   });
   const url = `${DRYAD_BASE_API_URL}/datasets?${searchQueryParams.toString()}`;
-  const response = await pRetry(
-    () =>
-      fetch(url, {
+  const responseData = await pRetry(
+    async (): Promise<unknown> => {
+      const response = await fetch(url, {
         headers: {
           Authorization: `Bearer ${accessToken}`,
         },
-      }),
+      });
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch datasets: ${response.status} ${response.statusText}`,
+        );
+      }
+
+      const data = await response.json();
+      return data;
+    },
     {
       retries: 1,
       onFailedAttempt: (error) => {
-        console.warn(
-          `Failed to fetch datasets: ${error.message}, retrying once...`,
-        );
+        console.warn(`${error.message}, retrying once...`);
       },
     },
   );
 
-  if (!response.ok) {
-    throw new Error(
-      `Failed to fetch datasets: ${response.status} ${response.statusText}`,
-    );
-  }
-
-  const rawData = await response.json();
-  const zodResult = DatasetResponseSchema.safeParse(rawData);
+  const zodResult = DatasetResponseSchema.safeParse(responseData);
   if (!zodResult.success) {
     console.warn(`Zod validation failed for ${url}`);
-    console.warn(JSON.stringify(rawData, null, 2));
+    console.warn(JSON.stringify(responseData, null, 2));
     throw zodResult.error;
   }
   return zodResult.data;
--- src/dryad/listFiles.ts
+++ src/dryad/listFiles.ts
@@ -1,5 +1,6 @@
 import { fetchToken } from "./fetchToken";
 import { FilesResponseSchema, type FilesResponse } from "./schemas";
+import pRetry from "p-retry";
 
 const DRYAD_BASE_API_URL = "https://datadryad.org/api/v2";
 type Params = {
@@ -9,20 +10,31 @@ export async function listFiles({ version }: Params): Promise<FilesResponse> {
   const accessToken = await fetchToken();
 
   const url = `${DRYAD_BASE_API_URL}/versions/${version}/files`;
-  const response = await fetch(url, {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
+  const responseData = await pRetry(
+    async (): Promise<unknown> => {
+      const response = await fetch(url, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch file list: ${response.status} ${response.statusText}`,
+        );
+      }
+      const data = await response.json();
+      return data;
     },
-  });
+    {
+      retries: 1,
+      onFailedAttempt: (error) => {
+        console.warn(`${error.message}, retrying once...`);
+      },
+    },
+  );
 
-  if (!response.ok) {
-    throw new Error(
-      `Failed to fetch datasets: ${response.status} ${response.statusText}`,
-    );
-  }
-
-  const rawData = await response.json();
-  const zodResult = FilesResponseSchema.safeParse(rawData);
+  const zodResult = FilesResponseSchema.safeParse(responseData);
   if (!zodResult.success) {
     throw new Error(`Zod validation failed for ${url}: ${zodResult.error}`);
   }
