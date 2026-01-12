67b3c865ef224312f95802da9faefddd1062dcf5
markusenglund <markus.s.englund@gmail.com>
2026-01-12 15:04:32 +0100

    Count datasets linked and not linked

--- src/commands/openalexConnectDatasets.ts
+++ src/commands/openalexConnectDatasets.ts
@@ -12,11 +12,25 @@ program
   )
   .action(async () => {
     const datasets = await getDatasetsByDownloadStatus("completed");
+    const numDatasetsByResult = {
+      found: 0,
+      noneFound: 0,
+    };
     console.log(`Found ${datasets.length} datasets to connect.`);
     for (const dataset of datasets.slice(0, 100)) {
       const article = await getArticleFromDryadDataset(dataset);
-      logger.info(`${dataset.title} -> ${article?.title}`);
+      logger.info(`
+dd: ${dataset.title} ->
+oa: ${article?.title}`);
+      if (article) {
+        numDatasetsByResult.found++;
+      } else {
+        numDatasetsByResult.noneFound++;
+      }
     }
+    console.log(
+      `Results: ${numDatasetsByResult.found} found, ${numDatasetsByResult.noneFound} none found`,
+    );
   });
 
 program.parse();
