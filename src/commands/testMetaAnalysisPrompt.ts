92521f5bfbfd02801cc7192badf713c797b96e20
markusenglund <markus.s.englund@gmail.com>
2026-03-28 17:50:34 +0100

    Add test-cases

---
+++ src/commands/testMetaAnalysisPrompt.ts
@@ -0,0 +1,110 @@
+interface TestCase {
+  extId: number;
+  isMetaAnalysis: boolean;
+}
+
+const metaAnalyses: TestCase[] = [
+  {
+    extId: 52841,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 24008,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 26649,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 19966,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 4763,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 52636,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 25928,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 26448,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 81273,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 121567,
+    isMetaAnalysis: true,
+  },
+  {
+    extId: 70773,
+    isMetaAnalysis: true,
+  },
+];
+
+const nonMetaAnalyses: TestCase[] = [
+  {
+    extId: 139667,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 146898,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 8685,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 26305,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 92897,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 154992,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 155108,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 90062,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 16328,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 155414,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 142655,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 146873,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 9683,
+    isMetaAnalysis: false,
+  },
+  {
+    extId: 6408,
+    isMetaAnalysis: false,
+  },
+];
